b477febbe99398117375f6dbeba154c444485f0c
Arvindiyengar <arvi.iyengar@gmail.com>
2016-12-14 16:28:36 +0530

    Issue fixed Doesn't do paging

--- script_getOpenissues.py
+++ script_getOpenissues.py
@@ -8,36 +8,46 @@ import json
 import datetime
 
 githubLink=sys.argv[1]
+count={"24hrs":0,"1week":0,"greater1week":0}
+
 
 a=githubLink[8:].split('/')
+page_id=1
 #Generate the unique filename which can be consumed later on
 filename="curl_output_"+str(uuid.uuid4())+".json"
-curlString='curl -XGET https://api.github.com/repos/'+a[1]+'/'+a[2]+"/issues > "+filename
+curlString='curl -XGET https://api.github.com/repos/'+a[1]+'/'+a[2]+"/issues?page="+str(page_id)+"&per_page=100 > "+filename
 os.system(curlString)
 f=open(filename)
 json_object=json.load(f)
 
+while(len(json_object)>0):
 #Get the current datetime in ISO format 
-currentdate=datetime.datetime.now()
+	currentdate=datetime.datetime.now()
 
-count={"24hrs":0,"1week":0,"greater1week":0}
+	count={"24hrs":0,"1week":0,"greater1week":0}
 
-for a in json_object:
-	
-	d=datetime.datetime.strptime(a['created_at'],"%Y-%m-%dT%H:%M:%SZ")
-	current=currentdate-(d)
-	
-	if(current.days==0):
-		count["24hrs"]=count["24hrs"]+1
-	elif(current.days>7):
-		count["greater1week"]=count["greater1week"]+1
-		count["24hrs"]=count["24hrs"]+1
-	else:
-		count["1week"]=count["1week"]+1
+	for a in json_object:
+		
+		d=datetime.datetime.strptime(a['created_at'],"%Y-%m-%dT%H:%M:%SZ")
+		current=currentdate-(d)
+		
+		if(current.days==0):
+			count["24hrs"]=count["24hrs"]+1
+		elif(current.days>7):
+			count["greater1week"]=count["greater1week"]+1
+			count["24hrs"]=count["24hrs"]+1
+		else:
+			count["1week"]=count["1week"]+1
 
-f.close()
+	f.close()
 
-os.remove(filename)
+	os.remove(filename)
+	page_id=2
+	filename="curl_output_"+str(uuid.uuid4())+".json"
+	curlString='curl -XGET https://api.github.com/repos/'+a[1]+'/'+a[2]+"/issues?page="+str(page_id)+"&per_page=100 > "+filename
+	os.system(curlString)
+	f=open(filename)
+	json_object=json.load(f)
 print("\nOpenIssues count for Past 24 hrs : ",count["24hrs"])
 print("OpenIssues count for Past 1 week : ",count["1week"])
 print("OpenIssues count for greater than 1 week : ",count["greater1week"])
